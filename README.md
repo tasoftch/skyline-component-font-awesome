a7a344a3370411b58cbba1a21413a90fe9f2744c
Thomas Abplanalp <info@tasoft.ch>
2020-04-29 17:21:01 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Skyline CMS Font Awesome
+The Skyline CMS Font Awesome Component includes the font awesome icons from www.fontawesome.com
---
+++ components.cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+use Skyline\Component\Config\CSSComponent;
+
+return [
+    "FontAwesome" => [
+        'css' => new CSSComponent("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", 'all', "sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0="),
+    ]
+];
