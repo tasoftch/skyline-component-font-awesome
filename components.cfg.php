0006ce3317592119dc4fa42f44d37958451eb805
Th. Abplanalp <info@tasoft.ch>
2020-08-28 18:03:58 +0200

    FIX: update

--- components.cfg.php
+++ components.cfg.php
@@ -6,9 +6,9 @@ use Skyline\Component\Config\CSSComponent;
 return [
     "FontAwesome" => [
         'css' => new CSSComponent(
-        	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.2.0/css/all.min.css",
+        	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css",
 			'all',
-			"sha512-6c4nX2tn5KbzeBJo9Ywpa0Gkt+mzCzJBrE1RB6fmpcsoN+b/w/euwIMuQKNyUoU/nToKN3a8SgNOtPrbW12fug==",
+			"sha512-KulI0psuJQK8UMpOeiMLDXJtGOZEBm8RZNTyBBHIWqoXoPMFcw+L5AEo0YMpsW8BfiuWrdD1rH6GWGgQBF59Lg==",
 			'anonymous'
 		)
     ]
