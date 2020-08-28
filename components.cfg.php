4ebbae0cbd6d22980982eefc600ff615237f3be5
Th. Abplanalp <info@tasoft.ch>
2020-08-28 18:00:39 +0200

    FIX: integrity

--- components.cfg.php
+++ components.cfg.php
@@ -2,8 +2,14 @@
 
 use Skyline\Component\Config\CSSComponent;
 
+
 return [
     "FontAwesome" => [
-        'css' => new CSSComponent("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", 'all', "sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0="),
+        'css' => new CSSComponent(
+        	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+			'all',
+			"sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==",
+			'anonymous'
+		)
     ]
 ];
